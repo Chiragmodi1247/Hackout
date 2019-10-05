953ebc6127b68bd8791ae4d8881a219627d6e07e
chiragmodi1247 <mchirag1247@gmail.com>
2019-10-05 15:02:00 +0530

    Initial commit

---
+++ src/Screens/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <div className="IndexHome">
+        <button>HomeScreen</button>
+      </div>
+    );
+  }
+}
+export default HomeScreen;
